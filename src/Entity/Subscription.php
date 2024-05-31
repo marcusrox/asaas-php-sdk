7b9930d0f27a6dd7f1c40b283c32164c6f5a57da
Adriano Carrijo <adrianovieirac@gmail.com>
2024-05-31 18:09:12 -0300

    improvements on subscriptions class

--- src/Entity/Subscription.php
+++ src/Entity/Subscription.php
@@ -10,7 +10,20 @@ namespace Adrianovcar\Asaas\Entity;
  */
 final class Subscription extends AbstractEntity
 {
-    public ?int $id;
+    const BILLING_TYPE_USER_CHOICE = 'UNDEFINED';
+    const BILLING_TYPE_CREDIT_CARD = 'CREDIT_CARD';
+    const BILLING_TYPE_BOLETO = 'BOLETO';
+    const BILLING_TYPE_PIX = 'PIX';
+
+    const CYCLE_WEEKLY = 'WEEKLY';
+    const CYCLE_BIWEEKLY = 'BIWEEKLY';
+    const CYCLE_MONTHLY = 'MONTHLY';
+    const CYCLE_BIMONTHLY = 'BIMONTHLY';
+    const CYCLE_QUARTERLY = 'QUARTERLY';
+    const CYCLE_SEMIANNUALLY = 'SEMIANNUALLY';
+    const CYCLE_YEARLY = 'YEARLY';
+
+    public ?string $id;
     /**
      * @var string Required field
      */
@@ -28,10 +41,10 @@ final class Subscription extends AbstractEntity
      * First installment due date
      * @var string Required field
      */
-    public string $nextDueDate;
-    public Discount $discount;
-    public Interest $interest;
-    public Fine $fine;
+    public ?string $nextDueDate;
+    public ?Discount $discount;
+    public ?Interest $interest;
+    public ?Fine $fine;
     /**
      * Required field
      * @var string "WEEKLY", "BIWEEKLY", "MONTHLY", "BIMONTHLY", "QUARTERLY", "SEMIANNUALLY" or "YEARLY"
@@ -41,23 +54,23 @@ final class Subscription extends AbstractEntity
     /**
      * @var string Installments should be paid until this date
      */
-    public string $endDate;
+    public ?string $endDate;
     /**
      * @var int Maximum number of payments to be created for this subscription
      */
-    public int $maxPayments;
-    public string $externalReference;
-    public array $split; // Todo: Implement split feature
-    public array $callback; // Todo: Implement callback feature
+    public ?int $maxPayments;
+    public ?string $externalReference;
+    public ?array $split; // Todo: Implement split feature
+    public ?array $callback; // Todo: Implement callback feature
     /**
      * @var CreditCard Required if billingType is CREDIT_CARD
      */
-    public CreditCard $creditCard;
+    public ?CreditCard $creditCard;
     /**
      * @var CreditCardHolderInfo Required if billingType is CREDIT_CARD
      */
-    public CreditCardHolderInfo $creditCardHolderInfo;
-    public string $creditCardToken;
+    public ?CreditCardHolderInfo $creditCardHolderInfo;
+    public ?string $creditCardToken;
     /**
      * IP from where the customer is making the purchase. It should not be the IP of your server.
      * @var string Required
@@ -70,6 +83,19 @@ final class Subscription extends AbstractEntity
     protected string $dateCreated;
     protected string $paymentLink;
     protected string $deleted;
+
+    public function __construct($parameters = null)
+    {
+        parent::__construct($parameters);
+
+        $this->nextDueDate = $this->nextDueDate ?? date('Y-m-d');
+        $this->remoteIp = $this->remoteIp ??
+            $_SERVER['REMOTE_ADDR'] ??
+            $_SERVER['HTTP_CLIENT_IP'] ??
+            $_SERVER['HTTP_X_FORWARDED_FOR'] ??
+            $_SERVER['HTTP_X_FORWARDED'] ??
+            '0.0.0.0.';
+    }
 
     /**
      * @param  string  $nextDueDate
