112951a69aee68328d7415c1453d67b8c4dd75e6
Adriano Carrijo <adrianovieirac@gmail.com>
2024-05-31 18:10:42 -0300

    credit card entity improvements (fill methods)

--- src/Entity/CreditCard.php
+++ src/Entity/CreditCard.php
@@ -2,6 +2,8 @@
 
 namespace Adrianovcar\Asaas\Entity;
 
+use function Pest\Faker\fake;
+
 /**
  * Credit Card Entity
  *
@@ -19,4 +21,36 @@ final class CreditCard extends AbstractEntity
     public string $expiryYear;
     public string $ccv;
 
+    /**
+     * This method fill the entity with a wrong credit card, because when you are on sandbox environment
+     * all credit card are approved, so this method is usefully to test the error flow
+     *
+     * @param  string  $type
+     * @return void
+     */
+    public function fillWrong(string $type = 'Visa'): CreditCard
+    {
+        $this->number = $type === 'Visa' ? '4916561358240741' : '5184019740373151';
+        $this->holderName = fake('pt_BR')->name();
+        $this->expiryMonth = date('m');
+        $this->expiryYear = date('Y');
+        $this->ccv = '123';
+
+        return $this;
+    }
+
+    /**
+     * Use the follow credit card info to have your transaction approved. (only in Sandbox environment)
+     * @return $this
+     */
+    public function fill(): CreditCard
+    {
+        $this->number = '4444 4444 4444 4444';
+        $this->holderName = fake('pt_BR')->name();
+        $this->expiryMonth = date('m');
+        $this->expiryYear = date('Y', strtotime('+1 YEAR'));
+        $this->ccv = '123';
+
+        return $this;
+    }
 }
