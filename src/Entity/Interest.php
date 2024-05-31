e1c82bad9abf8450c24c3f4c92f65e56928d5793
Adriano Carrijo <adrianovieirac@gmail.com>
2024-05-31 11:59:01 -0300

    new entities CreditCard, CreditCardHolderInfo, Discount, Fine and Interest

---
+++ src/Entity/Interest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Adrianovcar\Asaas\Entity;
+
+/**
+ * Interest Entity
+ *
+ * @author Adriano Carrijo <adrianovieirac@gmail.com>
+ */
+final class Interest extends AbstractEntity
+{
+    /**
+     * Percentage of interest per month on the value of the invoice for payment after the due date.
+     * @var float
+     */
+    public float $value;
+}
