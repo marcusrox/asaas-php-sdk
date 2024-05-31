ca3650b4036e1153e6059507f091df09e3c8d3af
Adriano Carrijo <adrianovieirac@gmail.com>
2024-05-31 18:23:42 -0300

    'create a new subscription test

--- tests/Feature/AsaasTest.php
+++ tests/Feature/AsaasTest.php
@@ -2,6 +2,10 @@
 
 use Adrianovcar\Asaas\Adapter\GuzzleHttpAdapter;
 use Adrianovcar\Asaas\Asaas;
+use Adrianovcar\Asaas\Entity\CreditCard;
+use Adrianovcar\Asaas\Entity\CreditCardHolderInfo;
+use Adrianovcar\Asaas\Entity\Fine;
+use Adrianovcar\Asaas\Entity\Subscription as SubscriptionEntity;
 use function Pest\Faker\fake;
 
 global $asaas, $adapter, $customer;
@@ -10,7 +14,7 @@ test('avoid dd, dump, ray, ds')
     ->expect(['dd', 'dump', 'ray', 'ds'])
     ->not->toBeUsed();
 
-// replace to your access token
+// replace with your access token
 $accessToken = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwMDE2OTc6OiRhYWNoXzEzZmZmMDE0LWFhM2MtNDIwZS1iMmFmLTA4YzcwNjY4MDkxNA==';
 $adapter = new GuzzleHttpAdapter($accessToken);
 $asaas = new Asaas($adapter, 'sandbox');
@@ -32,6 +36,8 @@ test('list all users', function () use ($asaas, $adapter) {
             'company' => fake('pt_BR')->company(),
             'phone' => fake('pt_BR')->phoneNumber(),
             'mobilePhone' => fake('pt_BR')->phoneNumber(),
+            'postalCode' => '13045-135',
+            'addressNumber' => '605',
         ];
 
         $customer = $asaas->customer()->create($defaultData);
@@ -40,3 +46,44 @@ test('list all users', function () use ($asaas, $adapter) {
     expect($customer->id)
         ->not()->toBeNull();
 });
+
+test('create a new subscription', function () use ($asaas) {
+
+    $customers = $asaas->customer()->getAll();
+    $customer = $customers[0] ?? false;
+
+    $credit_card = (new CreditCard())->fill();
+    $credit_card_holder = new CreditCardHolderInfo();
+    $credit_card_holder->name = $credit_card->holderName;
+    $credit_card_holder->email = fake()->email();
+    $credit_card_holder->phone = fake('pt_BR')->phoneNumber();
+    $credit_card_holder->mobilePhone = fake('pt_BR')->phoneNumber();
+    $credit_card_holder->cpfCnpj = cpf::cpfRandom();
+    $credit_card_holder->addressNumber = fake()->buildingNumber();
+    $credit_card_holder->addressComplement = 'complemento';
+    $credit_card_holder->postalCode = '01153-000';
+
+    $subscription = new SubscriptionEntity();
+    $subscription->customer = $customer->id;
+    $subscription->billingType = SubscriptionEntity::BILLING_TYPE_CREDIT_CARD;
+    $subscription->value = 10.50;
+    $subscription->cycle = SubscriptionEntity::CYCLE_MONTHLY;
+    $subscription->description = 'Service subscription';
+    $subscription->externalReference = '334433';
+    $subscription->creditCard = $credit_card;
+    $subscription->creditCardHolderInfo = $credit_card_holder;
+    $subscription->fine = new Fine(['value' => 10, 'type' => 'PERCENTAGE']);
+
+    $new_subscription = $asaas->subscription()->create($subscription);
+
+    expect($new_subscription)
+        ->not()->toBeEmpty();
+});
+
+//
+//test('list all subscriptions', function () use ($asaas, $adapter) {
+//    $subscriptions = $asaas->subscription()->getAll();
+//
+//    expect($subscriptions)
+//        ->not()->toBeEmpty();
+//});
